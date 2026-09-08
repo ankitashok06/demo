39374a418f2372e6c0edea6b299948116d0ec4e6
ankitashok06 <ankitaak693@gmail.com>
2026-09-09 00:24:46 +0530

    Update new funtion

--- PraticeExample/Funtion.m
+++ PraticeExample/Funtion.m
@@ -9,3 +9,18 @@ end
 function SA = func_frosting(r,h)
   SA = pi*r.^2 + 2*pi*r*h;
 end
+
+%If a large number of fair N-sided dice are rolled, the 
+%average of the simulated rolls is likely to be close to the mean of 1,2,...
+%N i.e. the expected value of one die. 
+%For example, the expected value of a 6-sided die is 3.5. 
+%Given N, simulate 1e8 N-sided dice rolls by creating a vector of 1e8 uniformly 
+%distributed random integers. Return the difference between 
+%the mean of this vector and the mean of integers from 1 to N. 
+% <------------SOLUTION ------------->
+function dice_diff = loln(N)
+
+    rolls = randi(N, 1e8, 1);
+    dice_diff = mean(rolls) - mean(1:N);
+
+end
